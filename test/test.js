23a944166193e331746452431c8734877ae8b9ac
mike.hogan <mike.hgan@contino.io>
2017-07-24 10:44:01 +0100

    basic node naivgation,without schema

--- test/test.js
+++ test/test.js
@@ -5,6 +5,61 @@ const store = require("../lib/schemaStore.js").store;
 
 describe('document storage against schema', function() {
     it('should work with an empty doc and an empty schema', function() {
-        expect(store({},{})).to.deep.equal({});
+        expect(store("root",{},{}, {})).to.deep.equal({root:{}});
+    });
+
+    const schema = {
+        root:{
+            nestedOne:{
+                nestedTwo:{
+                    name:{
+                        type:"string"
+                    }
+                }
+            }
+        }
+    };
+
+    const expectedDocument = {
+        root:{
+            nestedOne:{
+                nestedTwo:{
+                    name:"fred"
+                }
+            }
+        }
+    };
+
+    it('should create nodes of any depth', function() {
+
+        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, {});
+        expect(storedDocument).to.deep.equal(expectedDocument);
+    });
+
+    it('should navigate nodes of any depth and replace existing node', function() {
+        const existingDocument = {
+            root:{
+                nestedOne:{
+                    nestedTwo:{
+                        name:"mary"
+                    }
+                }
+            }
+        };
+
+        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, existingDocument);
+        expect(storedDocument).to.deep.equal(expectedDocument);
+    });
+
+    it('should navigate nodes of any depth and create existing node', function() {
+        const existingDocument = {
+            root:{
+                nestedOne:{
+                }
+            }
+        };
+
+        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, existingDocument);
+        expect(storedDocument).to.deep.equal(expectedDocument);
     });
 });
