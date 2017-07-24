a37dba2156f412a15f5018ec03b41fb983952882
mike.hogan <mike.hgan@contino.io>
2017-07-24 12:09:14 +0100

    basic array navigation

---
+++ test/testArrayNodeNavigation.js
@@ -0,0 +1,62 @@
+"use strict";
+const expect = require("chai").expect;
+const store = require("../lib/schemaStore.js").store;
+
+
+describe('document storage against schema', function () {
+    const schema = {
+        root: {
+            nestedOne: {
+                nestedTwo: {
+                    type:"array"
+                }
+            }
+        }
+    };
+
+    it('should create nodes of any depth', function () {
+        const expectedDocument = {
+            root: {
+                nestedOne: {
+                    nestedTwo: [
+                        {name: "fred"},
+                        {name: "mary"}
+                    ]
+                }
+            }
+        };
+
+        let initialStore = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, {});
+        const storedDocument = store("root/nestedOne/nestedTwo", {name: "mary"}, schema, initialStore);
+        expect(storedDocument).to.deep.equal(expectedDocument);
+    });
+
+    it('should allow array indexing', function () {
+        const existingDocument = {
+            root: {
+                nestedOne: {
+                    nestedTwo: [
+                        {name: "barney"},
+                        {name: "fred"},
+                        {name: "breda"}
+                    ]
+                }
+            }
+        };
+
+        const expectedDocument = {
+            root: {
+                nestedOne: {
+                    nestedTwo: [
+                        {name: "barney"},
+                        {name: "mary"},
+                        {name: "breda"}
+                    ]
+                }
+            }
+        };
+
+        const storedDocument = store("root/nestedOne/nestedTwo[1]", {name: "mary"}, schema, existingDocument);
+        expect(storedDocument).to.deep.equal(expectedDocument);
+    });
+});
--- test/testBasicNodeNavigation.js
+++ test/testBasicNodeNavigation.js
@@ -4,10 +4,6 @@ const store = require("../lib/schemaStore.js").store;
 
 
 describe('document storage against schema', function () {
-    it('should work with an empty doc and an empty schema', function () {
-        expect(store("root", {}, {}, {})).to.deep.equal({root: {}});
-    });
-
     const schema = {
         root: {
             nestedOne: {
