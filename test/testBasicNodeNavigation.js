a153ac3d0876182ad1cecf6fb1d52572f08ab906
mike.hogan <mike.hgan@contino.io>
2017-07-24 11:18:04 +0100

    more tidy

--- test/testBasicNodeNavigation.js
+++ test/testBasicNodeNavigation.js
@@ -3,17 +3,17 @@ const expect = require("chai").expect;
 const store = require("../lib/schemaStore.js").store;
 
 
-describe('document storage against schema', function() {
-    it('should work with an empty doc and an empty schema', function() {
-        expect(store("root",{},{}, {})).to.deep.equal({root:{}});
+describe('document storage against schema', function () {
+    it('should work with an empty doc and an empty schema', function () {
+        expect(store("root", {}, {}, {})).to.deep.equal({root: {}});
     });
 
     const schema = {
-        root:{
-            nestedOne:{
-                nestedTwo:{
-                    name:{
-                        type:"string"
+        root: {
+            nestedOne: {
+                nestedTwo: {
+                    name: {
+                        type: "string"
                     }
                 }
             }
@@ -21,50 +21,48 @@ describe('document storage against schema', function() {
     };
 
     const expectedDocument = {
-        root:{
-            nestedOne:{
-                nestedTwo:{
-                    name:"fred"
+        root: {
+            nestedOne: {
+                nestedTwo: {
+                    name: "fred"
                 }
             }
         }
     };
 
-    it('should create nodes of any depth', function() {
-
-        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, {});
+    it('should create nodes of any depth', function () {
+        const storedDocument = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, {});
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
-    it('should navigate nodes of any depth and replace existing node', function() {
+    it('should navigate nodes of any depth and replace existing node', function () {
         const existingDocument = {
-            root:{
-                nestedOne:{
-                    nestedTwo:{
-                        name:"mary"
+            root: {
+                nestedOne: {
+                    nestedTwo: {
+                        name: "mary"
                     }
                 }
             }
         };
 
-        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, existingDocument);
+        const storedDocument = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, existingDocument);
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
-    it('should navigate nodes of any depth and create existing node', function() {
+    it('should navigate nodes of any depth and create existing node', function () {
         const existingDocument = {
-            root:{
-                nestedOne:{
-                }
+            root: {
+                nestedOne: {}
             }
         };
 
-        var storedDocument = store("root/nestedOne/nestedTwo",{name:"fred"},schema, existingDocument);
+        const storedDocument = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, existingDocument);
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
-    it('should reject paths that dont match schema', function() {
-        var storedDocument = store("root/nestedOne/nestedFive/nestedSix",{name:"fred"},schema, {});
+    it('should reject paths that dont match schema', function () {
+        const storedDocument = store("root/nestedOne/nestedFive/nestedSix", {name: "fred"}, schema, {});
         expect(storedDocument).to.equal("Invalid path 'root/nestedOne/nestedFive/nestedSix' at 'root/nestedOne/nestedFive'");
     });
 });
