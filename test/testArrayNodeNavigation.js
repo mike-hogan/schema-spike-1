8bdd2032c87271057b10a6e5bc2031e3a01a9735
mike.hogan <mike.hgan@contino.io>
2017-07-25 14:45:24 +0100

    pivoting to tree editor

--- test/testArrayNodeNavigation.js
+++ test/testArrayNodeNavigation.js
@@ -3,42 +3,159 @@ const expect = require("chai").expect;
 const store = require("../lib/schemaStore.js").store;
 
 
-describe('document storage against schema', function () {
-    const schema = {
-        root: {
-            nestedOne: {
-                nestedTwo: {
-                    type: "array"
-                }
-            }
+describe('array based storage against schema', function () {
+
+    const idGenerator = {
+        generateId : () => {
+            return "cannedId";
         }
     };
 
+    it('should enforce types against array elements', function () {
+        const schema = {
+            types: {
+                name: {
+                    fields: ["firstName", "lastName"],
+                    firstName: {type: "string"},
+                    lastName: {type: "string"}
+                },
+                address: {
+                    fields: ["postcode"],
+                    postcode: {type: "string"}
+                }
+            },
+            root: {
+                nestedOne: {
+                    nestedTwo: {
+                        type: "array",
+                        allowedTypes: ["name", "address"]
+                    }
+                }
+            }
+        };
+
+        let originalDocument = {};
+        store("root/nestedOne/nestedTwo", {address: {postcode: "W1 68U"}}, schema, originalDocument, idGenerator);
+        expect(originalDocument).to.deep.equal({
+            root: {
+                nestedOne: {
+                    nestedTwo: [
+                        {
+                            id:"cannedId",
+                            type:"address",
+                            uri:"root/nestedOne/nestedTwo/cannedId",
+                            address: {postcode: "W1 68U"}
+                        }
+                    ]
+                }
+            }
+        });
+
+        originalDocument = {};
+        store("root/nestedOne/nestedTwo", {name: {firstName: "Donald", lastName:"Duck"}}, schema, originalDocument, idGenerator);
+        expect(originalDocument).to.deep.equal({
+            root: {
+                nestedOne: {
+                    nestedTwo: [
+                        {
+                            id:"cannedId",
+                            type:"name",
+                            uri:"root/nestedOne/nestedTwo/cannedId",
+                            name: {firstName: "Donald", lastName:"Duck"}
+                        }
+                    ]
+                }
+            }
+        });
+
+        const errorMessage = store("root/nestedOne/nestedTwo", {alienNode: {age: 12}}, schema, {}, idGenerator);
+        expect(errorMessage).to.equal("Invalid array type at path 'root/nestedOne/nestedTwo'");
+    });
+
     it('should create nodes of any depth', function () {
+        const schema = {
+            types: {
+                name: {
+                    fields: ["name"],
+                    name: {type: "string"}
+                }
+            },
+            root: {
+                nestedOne: {
+                    nestedTwo: {
+                        type: "array",
+                        allowedTypes: ["name"]
+                    }
+                }
+            }
+        };
+
         const expectedDocument = {
             root: {
                 nestedOne: {
                     nestedTwo: [
-                        {name: "fred"},
-                        {name: "mary"}
+                        {
+                            "id": "cannedId",
+                            "name": "fred",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/cannedId"
+                        },
+                        {
+                            "id": "cannedId",
+                            "name": "mary",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/cannedId"
+                        }
                     ]
                 }
             }
         };
 
-        let initialStore = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, {});
-        const storedDocument = store("root/nestedOne/nestedTwo", {name: "mary"}, schema, initialStore);
+        let initialStore = store("root/nestedOne/nestedTwo", {name: "fred"}, schema, {},idGenerator);
+        const storedDocument = store("root/nestedOne/nestedTwo", {name: "mary"}, schema, initialStore, idGenerator);
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
-    it('should allow array indexing', function () {
+    it('should allow setting of individual array elements', function () {
+        const schema = {
+            types: {
+                name: {
+                    fields: ["name"],
+                    name: {type: "string"}
+                }
+            },
+            root: {
+                nestedOne: {
+                    nestedTwo: {
+                        type: "array",
+                        allowedTypes: ["name"]
+                    }
+                }
+            }
+        };
+
         const existingDocument = {
             root: {
                 nestedOne: {
                     nestedTwo: [
-                        {name: "barney"},
-                        {name: "fred"},
-                        {name: "breda"}
+                        {
+                            "id": "id1",
+                            "name": "fred",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id1"
+                        },
+                        {
+                            "id": "id2",
+                            "name": "barney",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id2"
+                        },
+                        {
+                            "id": "id3",
+                            "name": "breda",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id3"
+                        }
                     ]
                 }
             }
@@ -48,15 +165,30 @@ describe('document storage against schema', function () {
             root: {
                 nestedOne: {
                     nestedTwo: [
-                        {name: "barney"},
-                        {name: "mary"},
-                        {name: "breda"}
+                        {
+                            "id": "id1",
+                            "name": "fred",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id1"
+                        },
+                        {
+                            "id": "id2",
+                            "name": "mary",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id2"
+                        },
+                        {
+                            "id": "id3",
+                            "name": "breda",
+                            "type": "name",
+                            "uri": "root/nestedOne/nestedTwo/id3"
+                        }
                     ]
                 }
             }
         };
 
-        const storedDocument = store("root/nestedOne/nestedTwo[1]", {name: "mary"}, schema, existingDocument);
+        const storedDocument = store("root/nestedOne/nestedTwo/id2", {name: "mary"}, schema, existingDocument,idGenerator);
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
@@ -100,7 +232,7 @@ describe('document storage against schema', function () {
             }
         };
 
-        const storedDocument = store("root/nestedOne/nestedTwo[0]/nestedThreePointTwo", {name: "mary"}, schema, existingDocument);
+        const storedDocument = store("root/nestedOne/nestedTwo[0]/nestedThreePointTwo", {name: "mary"}, schema, existingDocument,idGenerator);
         expect(storedDocument).to.deep.equal(expectedDocument);
     });
 
@@ -120,8 +252,7 @@ describe('document storage against schema', function () {
 
         const existingDocument = {
             root: {
-                nestedOne: {
-                }
+                nestedOne: {}
             }
         };
 
@@ -140,8 +271,25 @@ describe('document storage against schema', function () {
             }
         };
 
-        const intermediateDocument = store("root/nestedOne/nestedTwo/nestedThreePointTwo", {name: "mary"}, schema, existingDocument);
-        const storedDocument = store("root/nestedOne/nestedTwo/nestedThreePointOne", {name: "fred"}, schema, intermediateDocument);
+        const intermediateDocument = store("root/nestedOne/nestedTwo/nestedThreePointTwo", {name: "mary"}, schema, existingDocument,idGenerator);
+        const storedDocument = store("root/nestedOne/nestedTwo/nestedThreePointOne", {name: "fred"}, schema, intermediateDocument,idGenerator);
         expect(storedDocument).to.deep.equal(expectedDocument);
+    });
+
+    it('should return invalid path message for nodes after array nodes', function () {
+
+        const schema = {
+            root: {
+                nestedOne: {
+                    nestedTwo: {
+                        type: "array",
+                        nestedThree: {}
+                    }
+                }
+            }
+        };
+
+        const storedDocument = store("root/nestedOne/nestedTwo/invalidPath/more", {name: "fred"}, schema, {},idGenerator);
+        expect(storedDocument).to.equal("Invalid path 'root/nestedOne/nestedTwo/invalidPath/more' at 'root/nestedOne/nestedTwo/invalidPath'");
     });
 });
---
+++ test/testTreeEditor.js
@@ -0,0 +1,159 @@
+"use strict";
+const expect = require("chai").expect;
+const TreeEditor = require("../lib/treeEditor").TreeEditor;
+
+describe('TreeWalker', function () {
+    const mortgageApplicationSchema = {
+        types: {
+            property: {
+                fields: ["name", "value"],
+                name: {type: "string"},
+                value: {type: "number"}
+            },
+            shares: {
+                fields: ["ticker", "numberOfShares", "value"],
+                ticker: {type: "string"},
+                numberOfShares: {type: "number"},
+                value: {type: "decimal"}
+            },
+            cash: {
+                fields: ["currency", "value"],
+                currency: {type: "string"},
+                value: {type: "number"}
+            },
+        },
+        applicant: {
+            personalDetails: {
+                fields: ["firstName", "lastName"],
+                firstName: {
+                    type: "string"
+                },
+                lastName: {
+                    type: "string"
+                }
+            },
+            address: {
+                fields: ["postcode", "houseNumber"],
+                postcode: {
+                    type: "string"
+                },
+                houseNumber: {
+                    type: "number"
+                }
+            }
+        },
+        collateral: {
+            type: "array",
+            allowedTypes: ["property", "shares", "cash"]
+        }
+    };
+
+    it('should follow path through object nodes of existing document', function () {
+        const mortgageApplication = {
+            applicant: {
+                personalDetails: {
+                    firstName: "Donald", lastName: "Duck"
+                }
+            }
+        };
+        new TreeEditor(mortgageApplicationSchema).setExistingNode(mortgageApplication,"/applicant/personalDetails", {
+            firstName: "Charlie", lastName: "Chaplin"
+        });
+        expect(mortgageApplication).to.deep.equal({
+            applicant: {
+                personalDetails: {
+                    firstName: "Charlie", lastName: "Chaplin"
+                }
+            }
+        });
+    });
+
+    it('should support removal of existing nodes', function () {
+        const mortgageApplication = {
+            applicant: {
+                personalDetails: {
+                    firstName: "Donald", lastName: "Duck"
+                }
+            }
+        };
+        new TreeEditor(mortgageApplicationSchema).removeExistingNode(mortgageApplication,"/applicant/personalDetails");
+        expect(mortgageApplication).to.deep.equal({
+            applicant: {}
+        });
+    });
+
+    it('should create path of object nodes in empty document', function () {
+        const mortgageApplication = {};
+        new TreeEditor(mortgageApplicationSchema).createNode(mortgageApplication,"/applicant/personalDetails", {
+            firstName: "Charlie", lastName: "Chaplin"
+        });
+        expect(mortgageApplication).to.deep.equal({
+            applicant: {
+                personalDetails: {
+                    firstName: "Charlie", lastName: "Chaplin"
+                }
+            }
+        });
+    });
+
+    it('should follow path through array nodes of existing document', function () {
+        const mortgageApplication = {
+            collateral: [
+                {type:"cash",id:"id-1",uri:"/collateral/id-1", cash:{currency:"GBP", value:1000}},
+                {type:"shares",id:"id-22",uri:"/collateral/id-22", shares:{ticker:"TSLA", numberOfShares:300,value:332.98}},
+                {type:"cash",id:"id-2",uri:"/collateral/id-2", cash:{currency:"USD", value:1400}}
+            ]
+        };
+        new TreeEditor(mortgageApplicationSchema).setExistingNode(mortgageApplication,"/collateral/id-22", {cash:{currency:"EUR", value:999}});
+        expect(mortgageApplication).to.deep.equal({
+                collateral: [
+                    {type:"cash",id:"id-1",uri:"/collateral/id-1", cash:{currency:"GBP", value:1000}},
+                    {type:"cash",id:"id-22",uri:"/collateral/id-22", cash:{currency:"EUR", value:999}},
+                    {type:"cash",id:"id-2",uri:"/collateral/id-2", cash:{currency:"USD", value:1400}}
+                ]
+            });
+    });
+
+    it('should create path through array nodes of empty document', function () {
+        const mortgageApplication = {};
+        new TreeEditor(mortgageApplicationSchema).createNode(mortgageApplication,"/collateral", {cash:{currency:"EUR", value:999}});
+        expect(mortgageApplication).to.deep.equal({
+            collateral: [
+                {type:"cash",id:"id-1",uri:"/collateral/id-1", cash:{currency:"EUR", value:999}},
+            ]
+        });
+    });
+
+    it('should delete array items of existing document', function () {
+        const mortgageApplication = {
+            collateral: [
+                {type:"cash",id:"id-1",uri:"/collateral/id-1", cash:{currency:"GBP", value:1000}},
+                {type:"shares",id:"id-22",uri:"/collateral/id-22", shares:{ticker:"TSLA", numberOfShares:300,value:332.98}},
+                {type:"cash",id:"id-2",uri:"/collateral/id-2", cash:{currency:"USD", value:1400}}
+            ]
+        };
+        new TreeEditor(mortgageApplicationSchema).removeExistingNode(mortgageApplication,"/collateral/id-22");
+        expect(mortgageApplication).to.deep.equal({
+            collateral: [
+                {type:"cash",id:"id-1",uri:"/collateral/id-1", cash:{currency:"GBP", value:1000}},
+                {type:"cash",id:"id-2",uri:"/collateral/id-2", cash:{currency:"USD", value:1400}}
+            ]
+        });
+    });
+
+
+    it('should enforce allowed array types when creating array nodes', function () {
+        const mortgageApplication = {};
+        try {
+            new TreeEditor(mortgageApplicationSchema).createNode(mortgageApplication,"/collateral", {
+                unknownCollateralType: {
+                    currency: "EUR",
+                    value: 999
+                }
+            });
+            expect.fail("should have gotten an exception");
+        } catch (e) {
+            expect(e.message).to.equal("Attempting to set invalid array type at '/collateral'")
+        }
+    });
+});
